ea490d7fca7943c69f552ed6887766b69177f3d1
Trevor Brooks <trevorism@gmail.com>
2023-12-23 18:35:17 -0500

    Update prompt app to use latest login page

--- src/app/cypress.config.js
+++ src/app/cypress.config.js
@@ -1,6 +1,5 @@
-const {defineConfig} = require('cypress')
-
-module.exports = defineConfig({
+import { defineConfig } from "cypress";
+export default defineConfig({
     e2e: {
         specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
         baseUrl: 'http://localhost:4173'
--- src/main/groovy/com/trevorism/Application.groovy
+++ src/main/groovy/com/trevorism/Application.groovy
@@ -9,7 +9,7 @@ import io.swagger.v3.oas.annotations.info.Info
 @OpenAPIDefinition(
         info = @Info(
                 title = "Prompt",
-                version = "0.2.0",
+                version = "0.3.0",
                 description = "Prompt for answers from users",
                 contact = @Contact(url = "https://trevorism.com", name = "Trevor Brooks", email = "tbrooks@trevorism.com")
         )
