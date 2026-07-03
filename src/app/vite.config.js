74e1a061be50408657343167d97c2dfac4859abc
Trevor Brooks <trevorism@gmail.com>
2026-07-02 23:13:24 -0400

    fix tailwind misconfig

--- src/app/vite.config.js
+++ src/app/vite.config.js
@@ -1,5 +1,6 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 import PropertiesReader from 'properties-reader';
 import axios from 'axios';
 
@@ -21,7 +22,7 @@ try {
 }
 
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [vue(), tailwindcss()],
     server: {
         host: "localhost",
         proxy: {
